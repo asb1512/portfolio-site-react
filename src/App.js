45af5c3e266e8fbd962eecceb4046f3575db1c47
asb1512 <andrew@olympicmedia.com>
2021-12-17 02:15:49 -0500

    building out homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+
+import bg from './images/red-blue-waves.jpg';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar />
+
+      <img
+        src={bg}
+        alt="Andrew Bourgeois – Full-Stack Developer"
+        className='home-bg'
+      />
+
+      <div className='home-bg-blur' />
+
     </div>
   );
 }
