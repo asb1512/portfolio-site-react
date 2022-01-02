11ddfa222c9218383f0f2a76c71d8869d587c58a
asb1512 <andrew@olympicmedia.com>
2022-01-02 10:23:33 -0500

    added animations

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import './App.css';
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring'
+import { useTransition, useSpring, animated, config } from 'react-spring'
 import Navbar from './Navbar';
 
-import bg from './images/red-blue-waves.jpg';
+import bg from './images/marbre-blanc.jpg';
 
 
 
 function App() {
 
-  return (
-    <div className="App">
+  const [show, set] = useState(false);
+  const transitions = useTransition(show, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    reverse: show,
+    delay: 0,
+    config: config.wobbly,
+    onRest: () => {
+      set(true);
+      setToggle(true);
+    },
+  });
 
+  const [toggle, setToggle] = useState(false);
+  const boxStyle = useSpring({
+    width: toggle ? '80%' : '5%',
+    height: toggle ? '80%' : '5%',
+    opacity: toggle ? 1 : 0,
+    config: { delay: 4000 },
+  });
+
+
+
+  return transitions(
+    (styles, item) => item && <animated.div style={styles} className="App">
       <Navbar />
 
       <img
@@ -20,10 +43,11 @@ function App() {
         className='home-bg'
       />
 
-      <animated.div className='home-bg-blur' />
-
-    </div>
-  );
+      <animated.div className="home-block frosted" style={boxStyle}>
+        bringing <span>code</span> to life
+      </animated.div>
+    </animated.div>
+  )
 }
 
 export default App;
