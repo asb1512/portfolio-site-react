d96f933483070b7f5403b950f892cf21cff0ed85
asb1512 <andrew@olympicmedia.com>
2022-01-03 00:44:39 -0500

    building out side menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,64 @@
 import React, { useState } from 'react';
-import Hamburger from 'hamburger-react'
+import Hamburger from 'hamburger-react';
+import { useSpring, animated } from 'react-spring';
 
 
 
-export default function Navbar() {
+export default function Navbar(props) {
 
   const [isOpen, setOpen] = useState(false)
+  const hamburgerStyle = useSpring({
+    opacity: isOpen ? 1 : 0,
+    color: isOpen ? '#fff' : '#000',
+  })
+  const circleMenuStyle = useSpring({
+    top: isOpen ? '0' : '-100',
+    right: isOpen ? '0' : '-100',
+    width: isOpen ? '35%' : '5%',
+    fill: 'url(#grad)'
+  })
 
   return (
-    <div className='navbar frosted-light-unrounded'>
-      <div className='navbar-left'>ANDREW BOURGEOIS <span>full-stack developer</span></div>
+    <>
+      <div className='navbar'>
+        <animated.div className='navbar-left' style={hamburgerStyle}>
+          ANDREW BOURGEOIS <span>full-stack developer</span>
+        </animated.div>
 
-      <Hamburger toggled={isOpen} toggle={setOpen} className="hamburger" />
-    </div>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          onClick={() => props.setMenu(true)}
+          className="hamburger"
+          color={isOpen ? "#fff" : "#000"}
+        />
+      </div>
+
+      <animated.svg
+        viewBox="-25 25 100 100"
+        xmlns="http://www.w3.org/2000/svg"
+        className='circle'
+        style={circleMenuStyle}
+      >
+        <defs>
+          <linearGradient 
+            id="grad" 
+            x1="0%" 
+            y1="0%" 
+            x2="100%" 
+            y2="0%"
+          >
+            <stop offset="0%" stopColor='rgba(70,70,70,.8)' stopOpacity='1' />
+            <stop offset="100%" stopColor='rgba(0,0,0,1)' stopOpacity='1'/>
+          </linearGradient>
+        </defs>
+        <circle
+          cx="50"
+          cy="50"
+          r="50"
+          
+        />
+      </animated.svg>
+    </>
   )
 }
