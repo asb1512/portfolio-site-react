6e5d097709079b62ab02786bcd6b663c0163f4fd
asb1512 <andrew@olympicmedia.com>
2022-01-04 23:02:40 -0500

    added animation

--- src/Tagline.js
+++ src/Tagline.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
-import { useTransition, useSpring, animated, config } from 'react-spring';
+import { useSpring, animated } from 'react-spring';
 
 
 
 export default function Tagline(props) {
 
-
-
   const helloStyle = useSpring({
     color: props.bgColor === 'black' ? '#FFF' : '#000',
+    opacity: props ? 1 : 0,
   })
   const descSpanStyle = useSpring({
     color: props.bgColor === 'red' || props.bgColor === 'gray' ? '#FFF' : '#FE3636',
@@ -27,20 +26,24 @@ export default function Tagline(props) {
       opacity: 0,
     },
   })
+  const taglineStyle = useSpring({
+    opacity: props.showTagline ? 1 : 0,
+    transform: props.showTagline ? 'translate(0px, 0px)' : 'translate(-650px, 0px)',
+  })
 
 
 
   return (
-    <animated.div className='tagline-cntr' style={helloStyle}>
+    <animated.div className='tagline-cntr' style={{...helloStyle, ...taglineStyle}}>
       <div className='tagline-hello'>Hello,</div>
 
       <animated.div className='tagline-desc'>
         I'm <animated.span style={{...descSpanStyle, ...nameStyle }}>Andrew Bourgeois</animated.span>
       </animated.div>
 
-      {/* <animated.div className='tagline-desc'>
-        I'm a <animated.span style={{...descSpanStyle, ...fullStackStyle}}>full-stack developer</animated.span>
-      </animated.div> */}
+      <animated.div className='tagline-subdesc'>
+        full-stack <animated.span style={{...descSpanStyle, ...fullStackStyle}}>developer</animated.span>
+      </animated.div>
     </animated.div>
   )
 }
