4eb558902e5510513be02dfaa7b36dfe8a06c97c
asb1512 <andrew@olympicmedia.com>
2022-01-05 15:04:53 -0500

    writing about section

--- src/About.js
+++ src/About.js
@@ -13,6 +13,10 @@ export default function About() {
       </p>
 
       <p>
+        Since a young age, I've always had a love for languages. This passion led me to pursue degrees in Linguistics and French & Francophone Studies at the University of Florida and eventually teach English as a second language at 'Portail Rouge', a French middle school in the city of Saint-Etienne.
+      </p>
+
+      <p>
         
       </p>
     </div>
