c5b463ad66576a89dfd261c335d4e9efe8ff7ec3
asb1512 <andrew@olympicmedia.com>
2022-01-09 03:23:19 -0500

    created Projects and Contact and accompanying stylesheets; built out animation on mount

---
+++ src/components/Projects.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import './Projects.css';
+
+import { useTransition, animated, config } from 'react-spring';
+
+
+
+export default function Projects() {
+
+  const [show, set] = useState(false);
+  const transitions = useTransition(show, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+    reverse: show,
+    delay: 0,
+    config: config.wobbly,
+    onRest: () => {
+      set(true);
+    },
+  });
+
+
+
+  return transitions(
+    (styles, item) => item && <animated.div style={styles} className='projects-cntr'>
+      currently under construction...
+    </animated.div>
+  )
+}
