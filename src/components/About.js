f7564abfb5031fe6d701258a3045cadbf4c2d641
asb1512 <andrew@olympicmedia.com>
2022-01-11 23:03:27 -0500

    styling; setting up custom scrollbars

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './About.css';
 
 import { useTransition, animated, config } from 'react-spring';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 import img from '../images/about.png';
 
@@ -25,35 +26,37 @@ export default function About() {
 
   return transitions(
     (styles, item) => item && <animated.div style={styles} className='about-cntr'>
-      <h1>about.</h1>
+      <Scrollbars>
+        <h1>about.</h1>
 
-      <p>
-        Hi, I'm Andrew. I love creating web experiences that are beautiful, functional and reliable.
-      </p>
+        <p className="about-content">
+          Hi, I'm Andrew. I love creating web experiences that are beautiful, functional and reliable.
+        </p>
 
-      <p>
-        Since a young age, I've always had a love for languages. This passion led me to pursue degrees in Linguistics and French & Francophone Studies at the University of Florida and eventually teach English as a second language at 'Portail Rouge', a French middle school in the city of Saint-Etienne.
-      </p>
+        <p className="about-content">
+          Since a young age, I've always had a love for languages. This passion led me to pursue degrees in Linguistics and French & Francophone Studies at the University of Florida and eventually teach English as a second language at 'Portail Rouge', a French middle school in the city of Saint-Etienne.
+        </p>
 
-      <p>
-        After my time abroad, I realized that I didn't want to pursue a career in academia and began to search for other opportunities. 
-      </p>
+        <p className="about-content">
+          After my time abroad, I realized that I didn't want to pursue a career in academia and began to search for other opportunities. 
+        </p>
 
-      <p>
-        After some time, I ended up at a digital marketing startup and quickly fell into graphic design.
-      </p>
+        <p className="about-content">
+          After some time, I ended up at a digital marketing startup and quickly fell into graphic design.
+        </p>
 
-      <p>
-        Isn't it funny that as we age, despite our best efforts, we end up becoming more and more like out parents. My mother, an artist and graphic designer and my father, a teacher.
-      </p>
+        <p className="about-content">
+          Isn't it funny that as we age, despite our best efforts, we end up becoming more and more like out parents. My mother, an artist and graphic designer and my father, a teacher.
+        </p>
 
-      <div className='about-img-cntr'>
-        <img
-          src={img}
-          alt="Andrew Bourgeois, full-stack developer"
-          className='about-img'
-        />
-      </div>
+        <div className='about-img-cntr'>
+          <img
+            src={img}
+            alt="Andrew Bourgeois, full-stack developer"
+            className='about-img'
+          />
+        </div>
+      </Scrollbars>
     </animated.div>
   )
 }
