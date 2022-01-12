8c4ef76b1c9289dee2a7b9240cc845ce3446d5eb
asb1512 <andrew@olympicmedia.com>
2022-01-11 23:56:48 -0500

    imported tile images and added to render statement

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,11 @@ import './Projects.css';
 
 import { useTransition, animated, config } from 'react-spring';
 
+import adSize from '../images/adsize.png';
+import linguaFranca from '../images/linguafranca.png';
+import parkList from '../images/parklist.png';
+import wtpWine from '../images/wtpwine.png';
+
 
 
 export default function Projects() {
@@ -25,7 +30,29 @@ export default function Projects() {
   return transitions(
     (styles, item) => item && <animated.div style={styles} className='projects-cntr'>
       <div className='projects-sub-cntr'>
-        
+        <img
+          src={linguaFranca}
+          alt="Lingua Franca: connecting language learners"
+          className='project-tile lingua-franca'
+        />
+
+        <img
+          src={adSize}
+          alt="Ad Size: providing easy-to-find dimensions for advertising professionals"
+          className='project-tile ad-size' 
+        />
+
+        <img
+          src={parkList}
+          alt="Park List: allowing nature lovers to track their visits in America's national parks"
+          className='project-tile park-list'
+        />
+
+        <img
+          src={wtpWine}
+          alt="We The People Wine: drink up! (a work in progress)"
+          className='project-tile wtp-wine'
+        />
       </div>
     </animated.div>
   )
